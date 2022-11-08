9c702c0daecc8f13c7cda8da4da41ee8d46692c8
TheOneAbove0 <114287643+TheOneAbove0@users.noreply.github.com>
2022-11-08 19:03:41 +0545

    Update MainNavigation.js

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -6,14 +6,14 @@ function MainNavigation() {
 
   return (
     <header className={classes.header}>
-      <div className={classes.logo}>React Meetups</div>
+      <div className={classes.logo}>Your Holiday</div>
       <nav>
         <ul>
           <li>
-            <Link href='/'>All Meetups</Link>
+            <Link href='/'>All Places</Link>
           </li>
           <li>
-            <Link href='/new-meetup'>Add New Meetup</Link>
+            <Link href='/new-meetup'>Add New Place</Link>
           </li>
         </ul>
       </nav>
